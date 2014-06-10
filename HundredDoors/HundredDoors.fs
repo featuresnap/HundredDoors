8042210489ff90c0d927dafca858cad6141b79ae
Patrick Kelly <patrick.kelly@sherwin.com>
2014-06-09 20:12:28 -0400

    initial commit

---
+++ HundredDoors/HundredDoors.fs
@@ -0,0 +1,40 @@
+﻿module HundredDoors
+
+type Door = 
+    |Closed 
+    |Open
+
+let makeClosedDoors = function 
+    |numDoors -> List.init numDoors (fun n -> Closed)
+    
+
+let toggleDoor = function 
+    |Closed -> Open
+    |Open -> Closed
+
+let toggleDoors interval doors = 
+    
+    let toggleNthDoor i door = 
+        if (i+1) % interval = 0
+        then toggleDoor door
+        else door
+
+    doors |> List.mapi toggleNthDoor 
+
+let walkDoors doors = 
+    let size = doors |> List.length
+    
+    let rec walkHelper currentIteration doors = 
+        let currentToggler = toggleDoors currentIteration
+        match currentIteration with 
+        |i when i = size -> currentToggler doors
+        |_ -> 
+            let nextInput = currentToggler doors
+            walkHelper (currentIteration + 1) nextInput
+
+    walkHelper 1 doors
+
+let finder n door = 
+    match door with 
+    |Open -> n
+    |_ -> 
---
+++ HundredDoorsTests/Tests.fs
@@ -0,0 +1,39 @@
+﻿module HundredDoorsTests
+
+open NUnit.Framework
+open FsUnit
+open HundredDoors
+
+[<Test>]
+let ``All doors are initially closed`` () = 
+    makeClosedDoors(3) |> should equal [Closed; Closed; Closed]
+
+[<Test>]
+let ``Toggle opens closed door`` () =
+    toggleDoor Closed |> should equal Open
+
+[<Test>]
+let ``Toggle  closes open door`` () =
+    toggleDoor Open |> should equal Closed
+
+[<Test>]
+let ``toggle every second door opens the correct doors`` () =
+    let toggleFunction = toggleDoors 2
+    toggleFunction [Closed; Closed; Closed; Closed]
+        |> should equal [Closed; Open; Closed; Open] 
+
+[<Test>]
+let ``toggle every third door closes the correct doors`` () =
+    let toggleFunction = toggleDoors 3
+    toggleFunction [Open; Open; Open; Open; Open; Open]
+        |> should equal [Open; Open; Closed; Open; Open; Closed;]
+        
+
+[<Test>]
+let ``Full walk of 4 doors produces correct result`` () =
+    let doors = makeClosedDoors 4
+    let manualDoorWalker = 
+        (toggleDoors 1) >> (toggleDoors 2) >> (toggleDoors 3) >>  (toggleDoors 4)
+    let expectedResult = doors |> manualDoorWalker
+    let actualResult = doors |> walkDoors
+    actualResult |> should equal expectedResult
